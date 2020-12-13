3bc9ec265a83c18c3b2b70a77b2c462780e0298c
Laisa Costa <37801437+Azzyew@users.noreply.github.com>
2020-12-13 15:08:42 -0300

    Update Spanish text

--- src/pages/Spanish/index.jsx
+++ src/pages/Spanish/index.jsx
@@ -5,10 +5,10 @@ const Spanish = () => {
         <div className="spanish">
             <h1>La evolución de la comunicación</h1>
             <br/>
-            <p>La comunicación es muy importan para la raza humana, sin ella non teníamos sucedido en llegar rondé dónde estamos, por muy tiempo la comunicación fui lo único medio que nos humanas tiñamos de vencer los desafíos que tiñamos, debido a nuestra inteligencia y comunicación conseguimos llegar en la cima de la pirámide y evolucionar nuestras habilidades.</p>
-            <p>Sin duda una de las cosas que más evoluta fue la comunicación, por ejemplo la creación de la escritura, que hasta hoy es el periodo más impactante de la historia de la raza humana, algo que estamos estudiando hasta hoy y aprendiendo cosas nuevas todos los días. </p>
-            <p>Fuimos evolucionando hasta la popularización de la literatura no muy tiempo después de la invención de la escritura algo muy natural para la raza humana dado nuestra voluntad de contar historias, la literatura fue un proceso natural.</p>
-            <p>Mismo leyendo las historias antiguas podemos traer cosas nuevas y aprender, por eso estudiamos historia por ejemplo, es importante leer libros antiguos por varios motivos especialmente para aprender más sobre el contexto histórico de la época y también por qué mismo el libro siendo antiguo los consejos pueden ser aprovechadas.</p>
+            <p>La comunicación es muy importante para la raza humana, sin ella no tendríamos llegado en llegar dónde estamos, por mucho tiempo la comunicación ha sido el único medio que nosotros humanos teníamos de vencer los desafíos que teníamos, debido a nuestra inteligencia y comunicación conseguimos llegar en la cima de la pirámide y evolucionar nuestras habilidades.</p>
+            <p>Sin duda una de las cosas que más evolucionó fue la comunicación, por ejemplo la creación de la escritura, que hasta hoy es el periodo más impactante de la historia de la raza humana, algo que estamos estudiando hasta hoy y aprendiendo cosas nuevas todos los días.</p>
+            <p>Fuimos evolucionando hasta la popularización de la literatura no hace mucho después de la invención de la escritura algo muy natural para la raza humana dado nuestra voluntad de contar historias, la literatura fue un proceso natural.</p>
+            <p>Mismo leyendo las historias antiguas podemos traer cosas nuevas y aprender, por eso estudiamos historia por ejemplo, es importante leer libros antiguos por varios motivos especialmente para aprender más sobre el contexto histórico de la época y también "porque aunque el libro sea antiguo" los consejos pueden ser aprovechados.</p>
         </div>
     );
 }
